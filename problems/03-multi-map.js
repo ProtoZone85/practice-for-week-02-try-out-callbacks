e5de055ed097e18fb2ade433de1a3f84016b06f9
Your Name <email@example.com>
2022-11-02 21:32:16 -0500

    Andrew - commit - 5

--- problems/03-multi-map.js
+++ problems/03-multi-map.js
@@ -20,10 +20,25 @@ let result3 = multiMap("hi", 5, function(s) {
 });
 console.log(result3); // hi!!!!!
 *******************************************************************************/
+//Inputs: value, cb
+//Output: run in a cb * n
+//Create callback function that multiplies value by 10  --
+//Create a function - multimap
+//invoke the callback n times using for loop
+//return new value
+let multiplyNumber = (num) => num * 10;
 
 function multiMap(val, n, cb) {
   // Your code here
+  let newNum;
+  for(let i = 1; i <= n; i++){
+       console.log(val = cb(val));
+    // otherVariable = newNum
+  }
+  return val;
 }
+
+console.log(multiMap(5,3,multiplyNumber));
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
